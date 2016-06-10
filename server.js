e2fbb6d05a8fa37aec34b34c9ddeb327958c5580
Matin Fazli <matin.fazli@gmail.com>
2016-06-10 11:52:24 +0200

    tweaks

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ module.exports = {
     const indexPath = path.join(__dirname, '/../index.html');
     const publicPath = path.join(__dirname, '../public');
 
-    app.use('/public', publicPath)
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
+    app.use('/public', publicPath);
+    app.get('/', function (_, res) { res.sendFile(indexPath) });
 
-    return app
+    return app;
   }
 }
